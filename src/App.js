8a076efd5f2ec06bd7786e26b54efa31444f768a
Saska <mikic.saska@gmail.com>
2023-04-26 14:39:28 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './App.css'
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+
+
+export default function App() {
+   
+    return (
+        <>
+            <Header />
+            <Meme />   
+        </>
+    )
+}
